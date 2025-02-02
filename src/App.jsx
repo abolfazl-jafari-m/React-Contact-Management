3276b9c44ff906f4266b236c765753eb93d2798c
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-02-02 19:09:48 +0330

    feat(header): Header component is desgined

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header/Header"
+
 function App() {
 
 
     return (
         <>
-
+            <div className="w-full h-screen bg-gray-100">
+                <Header title={"وب اپلیکشن مدیریت مخاطبین"} />
+            </div>
         </>
     )
 }
