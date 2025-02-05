20a23178e7131bbab5d385232ca12b8325c70181
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-02-05 17:46:54 +0330

    feat(contacts): add a empty state png

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import ContactCard from "./ContactCard/ContactCard";
 import { toast, Slide } from "react-toastify";
 import { getContacts } from "../../services/contact";
+import empty from "../../assets/Images/empty-state.png"
 
 function Contacts({contacts , setContacts}) {
   useEffect(() => {
@@ -20,7 +21,15 @@ function Contacts({contacts , setContacts}) {
       <div className=" p-4" >
         <h2 className="font-bold text-lg text-center">لیست کاربران</h2>
         <div className="grid grid-cols-2 gap-5 bg-gray-200 p-3 rounded-md mt-5 max-h-[500px]  overflow-y-scroll no-scrollbar ">
-          {contactItems}
+          {
+            contactItems.length
+              ? contactItems
+              : <div className="flex items-center justify-center flex-col p-3 w-full col-span-full">
+                <img src={empty} alt="empty" className="w-3/5" />
+                    <p>برای شروع یک مخاطب اضافه کنید</p>
+              </div>
+          }
+          
         </div>
       </div>
     </>
