b22b4fbcdbacb04f1f0cdb1cf144a74538bf7844
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-02-07 21:20:52 +0330

    feat(contactCard) : adding toast to delete

--- src/components/Contacts/ContactCard/ContactCard.jsx
+++ src/components/Contacts/ContactCard/ContactCard.jsx
@@ -4,27 +4,40 @@ import Button from "../../shared/Button/Button";
 import { deleteContact, getContact } from "../../../services/contact";
 import Modal from "../../Modal/Modal";
 import Loading from "../../Loading/Loading";
+import { toast, Slide } from "react-toastify";
 
-function ContactCard({ contact , setContacts }) {
+function ContactCard({ contact, setContacts }) {
   const [deleteModal, setDeleteModal] = useState(false);
-  const [editModal , setEditModal]=useState(false);
-  const [isloading , setIsLoading]= useState(false);
-  const deleteBtnHandler = () =>{
+  const [editModal, setEditModal] = useState(false);
+  const [isloading, setIsLoading] = useState(false);
+  const deleteBtnHandler = () => {
     setIsLoading(true);
     deleteContact(contact.id)
-      .then((res)=>{
-        if(res){
-              setContacts((c)=> {
-                return c.filter((item)=> item.id !== contact.id)
-              })
+      .then((res) => {
+        if (res) {
+          toast.success("مخاطب با موفقیت حذف شد", {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Slide,
+          });
+          setContacts((c) => {
+            return c.filter((item) => item.id !== contact.id);
+          });
         }
-      }).finally(()=>{
-        setIsLoading(false)
       })
-  }
-  const editBtnHandler =()=>{
-    setEditModal(true)
-  }
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  const editBtnHandler = () => {
+    setEditModal(true);
+  };
   return (
     <>
       <div className="rounded-md bg-gray-300 px-6 py-3 flex flex-col gap-4">
@@ -71,13 +84,15 @@ function ContactCard({ contact , setContacts }) {
           </div>
         </div>
       )}
-      {
-        editModal && <Modal setContacts={setContacts} contact={contact} setVisibility={setEditModal}/>
-      }
-      
-      {
-        isloading && <Loading />
-      }
+      {editModal && (
+        <Modal
+          setContacts={setContacts}
+          contact={contact}
+          setVisibility={setEditModal}
+        />
+      )}
+
+      {isloading && <Loading />}
     </>
   );
 }
